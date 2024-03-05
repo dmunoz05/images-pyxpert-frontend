b1dac15726025f1ea56faecc4d74bfe382aa731c
= <daniel20025febrero29@gmail.com>
2024-03-05 00:28:09 -0500

    Add graphics

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,11 +7,12 @@ import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { PhotoResponse } from '../../types/image.type';
 import { HomeService } from './home.service';
+import { NgApexchartsModule } from "ng-apexcharts";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, SlibarComponent, RouterLink],
+  imports: [CommonModule, HeaderComponent, SlibarComponent, RouterLink, NgApexchartsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -20,8 +21,9 @@ export class HomeComponent implements OnInit {
   showPhoto = signal<boolean>(false);
   dataPhoto = {} as PhotoResponse;
   userInfo?: any
+  basicChart: any;
 
-  constructor(public loginService: LoginService, public http: HttpClient, private homeService: HomeService) { }
+  constructor(private loginService: LoginService, private http: HttpClient, private homeService: HomeService) { }
 
   ngOnInit() {
     //Obtener datos de usuario
@@ -45,4 +47,59 @@ export class HomeComponent implements OnInit {
     debugger
     console.log(changes)
   }
+
+  //Grafica
+  public chartOptions: any = {
+    series: [
+      {
+        name: 'Sales',
+        data: [35, 70, 125]
+      }
+    ],
+    chart: {
+      type: 'bar',
+      height: 350
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993]
+    },
+    title: {
+      text: 'Bar Chart',
+      align: 'left'
+    }
+  };
+  // @Input() title: ApexTitleSubtitle;
+  // @Input() chart: ApexChart;
+  // @Input() series: ApexAxisChartSeries | ApexNonAxisChartSeries;
+  // @Input() annotations: ApexAnnotations;
+  // @Input() colors: string[];
+  // @Input() dataLabels: ApexDataLabels;
+  // @Input() series: ApexAxisChartSeries | ApexNonAxisChartSeries;
+  // @Input() stroke: ApexStroke;
+  // @Input() labels: string[];
+  // @Input() legend: ApexLegend;
+  // @Input() fill: ApexFill;
+  // @Input() tooltip: ApexTooltip;
+  // @Input() plotOptions: ApexPlotOptions;
+  // @Input() responsive: ApexResponsive[];
+  // @Input() xaxis: ApexXAxis;
+  // @Input() yaxis: ApexYAxis | ApexYAxis[];
+  // @Input() grid: ApexGrid;
+  // @Input() states: ApexStates;
+  // @Input() title: ApexTitleSubtitle;
+  // @Input() subtitle: ApexTitleSubtitle;
+  // @Input() theme: ApexTheme;
+
+  // options = {
+  //   chart: {
+  //     type: 'line'
+  //   },
+  //   series: [{
+  //     name: 'sales',
+  //     data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+  //   }],
+  //   xaxis: {
+  //     categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+  //   }
+  // }
 }
