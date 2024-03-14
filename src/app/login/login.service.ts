96e4d9896c3e812fbaf31c2d6db8f3c87cc73922
= <daniel20025febrero29@gmail.com>
2024-03-14 16:34:34 -0500

    Environments variables

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,17 +1,17 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';
-import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
-import { environment } from '../../environments/environment.development';
-import { userInfo } from '../../types/user-info.type';
-import { listPhotos, MediaItems } from '../../types/list-photos.types';
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable, Subject } from 'rxjs'
+import { AuthConfig, OAuthService } from 'angular-oauth2-oidc'
+import { environment } from '../../environments/environment.development'
+import { userInfo } from '../../types/user-info.type'
+import { listPhotos, MediaItems } from '../../types/list-photos.types'
 
 const oAuthConfig: AuthConfig = {
-  issuer: environment.issuer,
-  strictDiscoveryDocumentValidation: environment.strictDiscoveryDocumentValidation,
-  redirectUri: environment.redirectUri,
-  clientId: environment.clientId,
-  scope: environment.scope,
+  issuer: environment.issuer || process.env['issuer'],
+  strictDiscoveryDocumentValidation: false,
+  redirectUri: environment.redirectUri || process.env['redirectUri'],
+  clientId: environment.clientId || process.env['clientId'],
+  scope: environment.scope || process.env['scope'],
 }
 
 @Injectable({
@@ -19,9 +19,9 @@ const oAuthConfig: AuthConfig = {
 })
 export class LoginService {
 
-  private readonly apiUrlDrive = 'https://www.googleapis.com/drive/v3';
+  private readonly apiUrlDrive = 'https://www.googleapis.com/drive/v3'
 
-  private readonly apiUrlFotos = 'https://photoslibrary.googleapis.com/v1';
+  private readonly apiUrlFotos = 'https://photoslibrary.googleapis.com/v1'
 
   constructor(private http: HttpClient, private oAuthService: OAuthService) { }
 
@@ -32,11 +32,11 @@ export class LoginService {
 
   loginWithGoogle() {
     if (typeof window !== 'undefined') {
-      this.oAuthService.configure(oAuthConfig);
+      this.oAuthService.configure(oAuthConfig)
       this.oAuthService.loadDiscoveryDocument().then(() => {
         this.oAuthService.tryLoginImplicitFlow().then(() => {
           if (!this.oAuthService.hasValidAccessToken()) {
-            this.oAuthService.initLoginFlow();
+            this.oAuthService.initLoginFlow()
           } else {
             if (this.isLoggedIn()) {
               this.getUserInfo()
@@ -49,29 +49,29 @@ export class LoginService {
 
   async getUserInfo(): Promise<any>{
     const userProfile = await this.oAuthService.loadUserProfile().then((userProfile) => {
-      this.userProfileSubject.next(userProfile as userInfo);
-      return userProfile;
+      this.userProfileSubject.next(userProfile as userInfo)
+      return userProfile
     })
     return userProfile
   }
 
   listFiles(): Observable<any> {
-    const url = `${this.apiUrlDrive}/files`;
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.oAuthService.getAccessToken()}`);
-    return this.http.get(url, { headers });
+    const url = `${this.apiUrlDrive}/files`
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.oAuthService.getAccessToken()}`)
+    return this.http.get(url, { headers })
   }
 
   listPhotos(): Observable<any> {
-    const url = `${this.apiUrlFotos}/mediaItems`;
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.oAuthService.getAccessToken()}`);
-    return this.http.get(url, { headers });
+    const url = `${this.apiUrlFotos}/mediaItems`
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.oAuthService.getAccessToken()}`)
+    return this.http.get(url, { headers })
   }
 
   isLoggedIn(): boolean {
-    return this.oAuthService.hasValidAccessToken();
+    return this.oAuthService.hasValidAccessToken()
   }
 
   signOut() {
-    this.oAuthService.logOut();
+    this.oAuthService.logOut()
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: '',
+  issuer: '',
+  strictDiscoveryDocumentValidation: false,
+  redirectUri: '',
+  clientId: '',
+  scope: ''
+}
